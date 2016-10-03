ca787e72fb31ab6504376ffa4eb1c82451f948ac
Jimmi Holst Christensen <rainbowhejsil@gmail.com>
2016-10-03 09:45:30 +0200

    first shot at a api for a lego nxt motor

--- prototype/prototype-3/prototype-3.ino
+++ prototype/prototype-3/prototype-3.ino
@@ -1,33 +1,79 @@
-void setup() {  
+enum Turning_Direction {
+  CLOCKWISE        = -1,
+  COUNTERCLOCKWISE = 1
+};
+
+struct Motor {
+  int pin = 0;
+  volatile int deg = 0;
+  Turning_Direction dir = CLOCKWISE;
+};
+
+Motor motor1;
+
+void setup() { 
+  motor1.pin = 12;
+
   // put your setup code here, to run once:
   Serial.begin(9200);
   
   pinMode(2, INPUT);
-  
   pinMode(3, INPUT);
+  pinMode(18, INPUT);
 
-  pinMode(12, OUTPUT);
+  pinMode(motor1.pin, OUTPUT);
 
-  attachInterrupt(0, int2, CHANGE); // 0 is interrupt number, listens at pin 2
-  attachInterrupt(1, int3, CHANGE); // 1 is interrupt number, listens at pin 3
+  //attachInterrupt(digitalPinToInterrupt(2), int2, CHANGE); // 0 is interrupt number, listens at pin 2
+  attachInterrupt(digitalPinToInterrupt(3), motor1_interrupt, CHANGE); // 1 is interrupt number, listens at pin 3
+  //attachInterrupt(digitalPinToInterrupt(18), button_clicked, CHANGE);
 
-  digitalWrite(12, HIGH);
+  //motor_turn(&motor1, CLOCKWISE);
+  //motor_stop(&motor1);
+  motor_turn_deg(&motor1, 360, CLOCKWISE);
 }
 
-int count = 0;
+void button_clicked() {
+  Serial.println("I was clicked!");
+  motor_stop(&motor1);
+}
+
 void int2() {
   // Serial.println("Int2");
 }
 
-void int3() {
-  count++;
-  if (count == 360) {
-    count = 0;
-    digitalWrite(12, LOW);
+void motor1_interrupt() {
+  motor1.deg += motor1.dir;
+}
+
+void motor_turn_deg(Motor* motor, int deg, Turning_Direction dir) 
+{
+  int goal = motor->deg + (deg * dir);
+
+  motor_turn(motor, dir);
+
+  switch (motor->dir) {
+    case CLOCKWISE:
+      while (goal <= motor->deg) ;
+      break;
+    case COUNTERCLOCKWISE:
+      while (goal >= motor->deg) ;
+      break;
   }
+  
+  motor_stop(motor);
+}
+
+void motor_stop(Motor* motor) 
+{
+  digitalWrite(motor->pin, LOW);
+}
+
+void motor_turn(Motor* motor, Turning_Direction dir) 
+{
+  motor->dir = dir;
+  digitalWrite(motor->pin, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(count);
 }
